ea35c70ec0e2424b8ae1ca16bd557eb1cb9e4e8b
RedaHamouche <reda.hamouche@outlook.com>
2020-03-03 02:12:44 +0100

    rotation added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let camera;
 let renderer;
 let scene;
 let universe;
+let viewZ = 0;
 
 function init() {
   container = document.querySelector(".scene");
@@ -22,6 +23,14 @@ function init() {
 
   camera.position.set(-8, 3, 25);
 
+  const ambient = new THREE.AmbientLight(0x404040, 2);
+
+  scene.add(ambient);
+
+  const light = new THREE.DirectionalLight(0xffffff, 5);
+  light.position.set(-1, 30, 3);
+  scene.add(light);
+
   // Renderer
 
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
@@ -35,8 +44,21 @@ function init() {
   let loader = new THREE.GLTFLoader();
   loader.load("scenes/scene.gltf", function(gltf) {
     scene.add(gltf.scene);
-    renderer.render(scene, camera);
+    universe = gltf.scene.children[0];
+    animate();
   });
 }
+
+function animate() {
+  requestAnimationFrame(animate);
+  universe.rotation.z = viewZ;
+  universe.rotation.y += 0.005;
+  renderer.render(scene, camera);
+}
+addEventListener("mousemove", e => {
+  viewZ += e.clientX / 10000;
+  console.log("hi");
+});
+
 init();
 console.log("linked");
