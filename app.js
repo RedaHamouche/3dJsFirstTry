941a465c9e8c3d3fb5c9da70ec6c7f9e4d3f8bbe
RedaHamouche <reda.hamouche@outlook.com>
2020-03-03 01:20:19 +0100

    js

--- app.js
+++ app.js
@@ -11,4 +11,25 @@ function init() {
 
   // Create scene
   scene = new THREE.Scene();
+
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 500;
+
+  // Camera setup
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+  camera.position.set(-50, 40, 350);
+
+  // Renderer
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  // Load Model
+
+  let loader = new THREE.GLTFLoader();
+  loader.load("./3d/scene.gtlf", function(gltf) {
+    scene.add(gltf.scene);
+  });
 }
