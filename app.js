7a26344250913f5de814e186d28a0377eab35b26
RedaHamouche <reda.hamouche@outlook.com>
2020-03-03 01:05:06 +0100

    first push

---
+++ app.js
@@ -0,0 +1,14 @@
+// Variables for setup;
+
+let container;
+let camera;
+let renderer;
+let scene;
+let universe;
+
+function init() {
+  container = document.querySelector(".scene");
+
+  // Create scene
+  scene = new THREE.Scene();
+}
